15fdade0ec292bbddc0bc2d43c8c680b65267b6e
ghdus4185 <ckdghdus@naver.com>
2019-10-28 10:50:08 +0900

    모델 설정

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.TextField()
+
+
+class Music(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.TextField()
+
+
+class Comment(models.Model):
+    music = models.ForeignKey(Music, on_delete=models.CASCADE)
+    content = models.TextField()
